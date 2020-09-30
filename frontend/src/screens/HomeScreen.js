151e84c2d46f2c5677efc5b2da2c19a96bfd4356
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-10-01 01:38:15 +0530

    Rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,8 +8,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map(product => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            {/* <h3>{product.name}</h3> */}
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
