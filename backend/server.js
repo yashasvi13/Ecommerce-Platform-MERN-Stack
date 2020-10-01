de1097c87a4fe2b54cf7a451b5b1122d8e663c06
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-10-01 17:59:53 +0530

    fetching products, product from react

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const products = require("./data/product");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
