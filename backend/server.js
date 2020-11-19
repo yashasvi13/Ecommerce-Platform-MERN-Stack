db38e50094913c76b1973e9763cab62cff79384e
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-11-19 16:11:04 +0530

    mongoDB: connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import connectDB from "./config/db.js";
 import products from "./data/product.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
