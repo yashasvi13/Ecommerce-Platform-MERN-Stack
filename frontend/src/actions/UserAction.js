f86d5b3c71194bfcfa5358cf4ba07115e6c7b150
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-11-21 15:01:06 +0530

    user in navbar, logout

--- frontend/src/actions/UserAction.js
+++ frontend/src/actions/UserAction.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-  USER_LOGIN_FAILED
+  USER_LOGIN_FAILED,
+  USER_LOGOUT
 } from "../constants/UserConstants";
 
 export const login = (email, password) => async dispatch => {
@@ -29,4 +30,9 @@ export const login = (email, password) => async dispatch => {
           : error.message
     });
   }
+};
+
+export const logout = () => dispatch => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
 };
