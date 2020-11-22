941e0fa53b96fdd992016c463647c81a7841f9aa
Yashasvi Jain <yashasvijain1303@gmail.com>
2020-11-22 11:43:06 +0530

    place order screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -23,6 +23,7 @@ const ShippingScreen = ({ history }) => {
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 />
+      <h1>Shipping Address</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
           <Form.Label>Address</Form.Label>
